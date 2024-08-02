caadeb4dcb3d6869c4f406da5adea04cfdc6b241
Ariel-Tr <arieltruter76@gmail.com>
2024-08-02 14:45:44 +0200

    Add CV download button
    still have to add CV as path

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  downloadFile() {
+    const fileUrl = 'assets/resume.pdf'; // Path to your file in the assets directory
+    const fileName = 'resume.pdf'; // Desired file name
 
+    // Create an anchor element and trigger the download
+    const link = document.createElement('a');
+    link.href = fileUrl;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 }
